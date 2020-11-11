cd18fa6291b27ff6a7f0d04a17ed396b0127ff01
B4ch0 <50328781+B4ch0@users.noreply.github.com>
2020-11-11 19:30:22 +0100

    Add files via upload

---
+++ src/views/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>LOGIN</h1>
+<div class="container col-md-2 col-md-offset-4">
+  <div class="row ">
+
+   <form>
+  <div class="form-group ">
+    <label for="user">User</label>
+    <input type="user" class="form-control" id="user"  placeholder="Username...">
+    </div>
+    
+
+
+  <div class="form-group">
+    <label for="pass">Password</label>
+    <input type="password" class="form-control" id="pass" placeholder="Password...">
+  </div>
+ 
+  <button type="submit" class="btn btn-success">Potvrdi</button>
+</form>
+  
+  
+  </div>
+</div></div>
+</template>
---
+++ src/views/signup.vue
@@ -0,0 +1,43 @@
+<template>
+  
+  <div class="signup">
+    <h1>SIGN UP</h1>
+<div class="container col-md-2 col-md-offset-4">
+  <div class="row ">
+
+   <form>
+  <div class="form-group ">
+    <label for="ime">Ime</label>
+    <input type="text" class="form-control" id="ime"  placeholder="Ime...">
+    
+  </div>
+<div class="form-group ">
+    <label for="prezime">Prezime</label>
+    <input type="text" class="form-control" id="prezime"  placeholder="Prezime...">
+    
+  </div>
+  <div class="form-group ">
+    <label for="email">E-Mail</label>
+    <input type="text" class="form-control" id="email"  placeholder="E-mail...">
+    
+  </div>
+
+
+<div class="form-group ">
+    <label for="user">Username</label>
+    <input type="text" class="form-control" id="user"  placeholder="Username...">
+    
+  </div>
+
+<div class="form-group">
+    <label for="pass">Password</label>
+    <input type="password" class="form-control" id="pass" placeholder="Password...">
+  </div>
+ 
+  <button type="submit" class="btn btn-success">Potvrdi</button>
+</form>
+  
+  
+  </div>
+</div></div>
+</template>
